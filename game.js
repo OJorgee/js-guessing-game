e0492647aed1a5387dd24ed7317b365ba5396a6c
Ana Grilo <metaspheres@gmail.com>
2026-06-22 11:04:17 +0100

    added function getPlayerGuess

--- game.js
+++ game.js
@@ -5,3 +5,27 @@ function generateRandomNumber()
     return Math.floor(Math.random() * 100) + 1
 }
 console.log(generateRandomNumber())
+
+function getPlayerGuess(){
+    let isValid = false
+    let guess
+    let number 
+    while (!isValid){
+
+    number = prompt("Please enter a number from 1 to 100:");
+
+     if (isNaN(number)){
+        console.log("Try again")
+     }
+     else if (number > 100 || number < 0 || number === ""){
+        console.log("Try again")
+     }
+     else{
+        return number
+        break
+     }
+ 
+    }
+}
+console.log(getPlayerGuess())
+
