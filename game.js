cb88df86d66644d68564dcd57dad30b59ecc544c
Sérgio <sergiopbejamartinho@gmail.com>
2026-06-22 10:11:57 +0100

    Created Genereate number for player to guess function

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+
+//Genereate number for player to guess
+function generateRandomNumber()
+{
+    return Math.floor(Math.random() * 100) + 1
+}
+console.log(generateRandomNumber())
